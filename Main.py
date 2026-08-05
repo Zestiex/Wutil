8b3935201411ae3b7001ef0264c869b20ff27876
Zestiex <Zestiex@proton.me>
2026-08-05 22:11:54 +0200

    'def new_employee():' Adds new employee with all checks, confirmations and try:except things type shi
    'def list_employees():' Gives all employees found in the dict in nice and clean way

--- Main.py
+++ Main.py
@@ -177,3 +177,62 @@ class Employee:
             print("\nError: Invalid input.")
 """
 
+import sys
+
+employees = {}
+
+def new_employee():
+    worker_id = input("\nID: ")
+
+    if not worker_id:
+        print("\nError: ID cannot be empty.")
+        return
+    elif worker_id in employees:
+        print("\nError: ID already exists.")
+        return
+
+    name = input("\nName: ")
+    if not name:
+        print("\nError: Name cannot be empty.")
+        return
+
+    role = input("\nRole: ")
+    if not role:
+        print("\nError: Role cannot be empty.")
+        return
+
+    try:
+        hours_str = int(input("\nHours: "))
+        tasks_str = int(input("\nTasks: "))
+    except ValueError:
+        print("\nError: Invalid input.")
+        return
+
+    print("\nEmployee Details:")
+    print(f"ID: {worker_id}\n"
+          f"Name: {name}\n"
+          f"Role: {role}\n"
+          f"Hours: {hours_str}\n"
+          f"Tasks: {tasks_str}")
+
+    while True:
+        confirm = input("\nConfirm? (y/n): ").lower()
+        if confirm == "y":
+            employees[worker_id] = {"name" : name,
+                "role" : role,
+                "hours" : hours_str,
+                "tasks" : tasks_str
+            }
+            print(f"\nEmployee {worker_id} has been added.")
+            break
+        elif confirm == "n":
+            print("\nEmployee creation cancelled.")
+            return
+        else:
+            print("\nError: Invalid input.")
+
+def list_employees():
+    print("\n{'ID':<6}{'Name':<15}{'Role':<15}{'Hours':<8}{'Tasks':<6}")
+    for w_id, employee in employees.items():
+        print(f"{w_id:<6}{employee['name']:<15}{employee['role']:<15}{employee['hours']:<8}{employee['tasks']:<6}")
+
