f11b8073917d46c4f896d8e082b6792018235d77
Zestiex <Zestiex@proton.me>
2026-08-06 17:52:10 +0200

    'def edit_emp()' still is work in progress but i added big chunk of main logic, a nice and helpfull list of ID when calling edit_emp() and fixed alot of issues and things that could cause errors

--- Main.py
+++ Main.py
@@ -46,7 +46,8 @@ def new_emp():
     while True:
         confirm = input("\nConfirm? (y/n): ").lower()
         if confirm == "y":
-            employees[worker_id] = {"name" : name,
+            employees[worker_id] = {"ID" : emp_id,
+                "name" : name,
                 "role" : role,
                 "hours" : hours_str,
                 "tasks" : tasks_str
@@ -60,6 +61,9 @@ def new_emp():
             print("\nError: Invalid input.")
 
 def edit_emp():
+    for emp_id in employees:
+        print(f"\nID: {emp_id}\n | Name: {employees[emp_id]['name']}")
+
     worker_id = input("\nID: ")
     if not worker_id:
         print("\nError: ID cannot be empty.")
@@ -68,38 +72,61 @@ def edit_emp():
         print("\nError: ID Does not exist.")
         return
 
-    print("\nEmployee Details:")
-    print(f"ID: {worker_id}\n"
-          f"Name: {name}\n"
-          f"Role: {role}\n"
-          f"Hours: {hours_str}\n"
-          f"Tasks: {tasks_str}")
+    emp = employees[worker_id]
+
+    print("\nEmployee Details:\n"
+        f"ID: {worker_id}\n"
+        f"Name: {emp['name']}\n"
+        f"Role: {emp['role']}\n"
+        f"Hours: {emp['hours_str']}\n"
+        f"Tasks: {emp['tasks_str']}\n"
+    )
 
     while True:
-        try:
-            select = int(input(
-                        "\nSelect an option\n"
-                        "1. Change name\n"
-                        "2. Change role\n"
-                        "3. change hours\n"
-                        "4. change tasks"
-                    )).lower()
-        except ValueError:
-            print("\nError: Invalid input.")
-            return
+        print(
+            "\nSelect an option\n"
+            "1. Change name\n"
+            "2. Change role\n"
+            "3. change hours\n"
+            "4. change tasks\n"
+            "e. exit\n"
+        )
 
+        select = input("").strip().lower()
         if select == "1":
-            name = input("\nName: ")
+            new_name = input("\nName: ").strip()
+            if not new_name:
+                print("\nError: Name cannot be empty.")
+                continue
 
+            employees[worker_id]["name"] = new_name
+            print(f"\nEmployee {worker_id} name has been succsesfully changed to {new_name}.")
+            continue
         elif select == "2":
-            pass
+            new_role = input("\nRole: ")
+            if not new_role:
+                print("\nError: Role cannot be empty.")
+                continue
+
+            employees[worker_id]["role"] = new_role
+            print(f"\nEmployee {worker_id} role has been succsesfully changed to {new_role}.")
+            continue
         elif select == "3":
             pass
         elif select == "4":
             pass
+        elif select == "e":
+            print("\nEmployee Details:\n"
+                  f"ID: {worker_id}\n"
+                  f"Name: {emp['name']}\n"
+                  f"Role: {emp['role']}\n"
+                  f"Hours: {emp['hours_str']}\n"
+                  f"Tasks: {emp['tasks_str']}\n"
+                  )
+            break
         else:
             print("\nError: Invalid input.")
-            return
+            continue
 
 def list_emps():
     print(f"\n{'ID':<6}{'Name':<15}{'Role':<15}{'Hours':<8}{'Tasks':<6}")
